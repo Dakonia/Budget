fcf00a8ae55d04d135d0a789dd981685c90ebadf
Vlad <vladmen13@mail.ru>
2024-04-02 11:07:31 +0300

    date

--- Budget/Main/models.py
+++ Budget/Main/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
+
 
 
 
@@ -17,7 +17,8 @@ class Expense(models.Model):
     category = models.ForeignKey(ExpenseCategory, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
-    created_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return self.description
@@ -35,7 +36,8 @@ class Income(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(IncomeCategory, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return f"Income of {self.amount} on {self.created_at}"
